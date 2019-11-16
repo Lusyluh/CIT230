c3025d10fc508e03dba5d49fb22d791bed427e14
Lusuluh <mav19003@byui.edu>
2019-11-16 23:49:01 +0200

    modified

--- lesson9/js/towns.js
+++ lesson9/js/towns.js
@@ -10,13 +10,14 @@ fetch(requestURL)
         const towns = jsonObject['towns'];
         
         for (let i = 0; i < towns.length;i++){
-            if (towns[i].name == "Preston" && towns[i].name == "Fish Haven" && towns[i].name == "Soda Springs"){
-                let card = document.createElement('section'); //each town section
+            if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs"){
+                let town = document.createElement('section'); //each town section
                 let h2 = document.createElement('h2')
-                let motto = document.createElement('h3')
+                let motto = document.createElement('h4')
                 let yearFounded = document.createElement('p')
                 let currentPopulation = document.createElement('p')
                 let averageRainfall = document.createElement('p')
+                let info = document.createElement('span')
                 let photo = document.createElement('img')
 
                 h2.textContent = towns[i].name;
@@ -24,15 +25,17 @@ fetch(requestURL)
                 yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
                 currentPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
                 averageRainfall.textContent = 'Annual Rain: ' + towns[i].averageRainfall;
-                photo.setAttribute('src', towns[i].photo);
+                photo.src = "images/" + towns[i].photo;
+                photo.alt = "Image of " + towns[i].name;
 
-                card.appendChild(h2);
-                card.appendChild(motto)
-                card.appendChild(yearFounded)
-                card.appendChild(currentPopulation)
-                card.appendChild(averageRainfall)
-                card.appendChild(photo)
-                document.querySelector('div.townData').appendChild(card);
+                info.appendChild(h2);
+                info.appendChild(motto);
+                info.appendChild(yearFounded);
+                info.appendChild(currentPopulation);
+                info.appendChild(averageRainfall);
+                town.appendChild(info);
+                town.appendChild(photo);
+                document.querySelector('div.townData').appendChild(town);
 
             }
             
