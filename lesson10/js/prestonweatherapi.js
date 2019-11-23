bb32e4e03fdc32108dc05d6a31d14a6a405b6dce
Lusuluh <mav19003@byui.edu>
2019-11-24 00:17:01 +0200

    updates

--- lesson10/js/prestonweatherapi.js
+++ lesson10/js/prestonweatherapi.js
@@ -9,8 +9,18 @@ fetch(apiURL)
         document.getElementById('temp').textContent = cwObject.main.temp_max;
         document.getElementById('windSpeed').textContent = cwObject.wind.speed;
         document.getElementById('humidity').textContent = cwObject.main.humidity;
+//wind chill calculations
+var temp = cwObject.main.temp;
+var ws = cwObject.wind.speed; //ws stands for wind speed
+if (temp <= 50 && ws >= 3 ){
+    var factor = 35.74 + 0.6215*temp - (35.75* Math.pow(ws,0.16)) + 0.4275 *temp*(Math.pow(ws,0.16));
+    document.getElementById('windChill').innerHTML = factor.toFixed(2) + "°F";
+}
+else {
+    document.getElementById('windChill').innerHTML = "N/A";
+} 
 
-    })
+    });
 const fivedayURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=cbd2cdfdd1f438f29af3417a193151ae&units=imperial';
 
 fetch(fivedayURL)
@@ -21,8 +31,8 @@ fetch(fivedayURL)
         var dayNum = 1;
         for( let i = 0; i < jsObject.list.length; i++){
         if(jsObject.list[i].dt_txt.includes("18:00:00")){
-            let uniTime = jsObject.list[i].dt;
-            let currentDate = new Date(uniTime * 1000);
+            let TimeOfData = jsObject.list[i].dt;
+            let currentDate = new Date(TimeOfData * 1000);
             let weekday = ['Sun','Mon','Tues','Wed','Thur','Fri','Sat'];
             let dayofWeek = weekday[currentDate.getDay()]
             document.getElementById('day'+ dayNum).textContent = dayofWeek;
@@ -31,7 +41,6 @@ fetch(fivedayURL)
             const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[i].weather[0].icon + '.png';
             const desc = jsObject.list[i].weather[0].description;
 
-            //document.getElementById('imagesrc').textContent = imagesrc;
             document.getElementById('icon'+ dayNum).setAttribute('src',imagesrc);
             document.getElementById('icon'+ dayNum).setAttribute('alt',desc);
             document.getElementById('temp' + dayNum).textContent = Math.round(jsObject.list[i].main.temp_max) + "°F";
