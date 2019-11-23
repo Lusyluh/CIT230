a7268e5e02f05950e810c41d894f118defbe5e53
Lusuluh <mav19003@byui.edu>
2019-11-23 05:28:41 +0200

    api

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=cbd2cdfdd1f438f29af3417a193151ae&units=imperial';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=cbd2cdfdd1f438f29af3417a193151ae&units=imperial';
 
 fetch(apiURL)
     .then((response) => response.json())
