32ed12d5da330e07756c9ada885d4a51a6ba8425
Lusuluh <mav19003@byui.edu>
2019-12-02 15:42:53 +0200

    finale

--- lesson11/js/date.js
+++ lesson11/js/date.js
@@ -2,8 +2,6 @@
 var updates = new Date();
 document.getElementById("currentYear").textContent = updates.getFullYear();
 
-
-
 document.getElementById("dateModi").textContent= document.lastModified;
 
 
