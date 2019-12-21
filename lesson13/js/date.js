b795a53efa8d690d2dbf58a9303ae2be8fdb1299
Lusuluh <mav19003@byui.edu>
2019-12-21 15:32:29 +0200

    progress

---
+++ lesson13/js/date.js
@@ -0,0 +1,12 @@
+
+let updates = new Date();
+
+document.getElementById("currentYear").textContent = updates.getFullYear();
+
+let modify = document.lastModified;
+
+document.getElementById("dateModi").textContent = modify;
+
+
+
+
