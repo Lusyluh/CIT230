64196613cb1ee960da9cc6202ec78afaa4b27bac
Lusuluh <mav19003@byui.edu>
2019-12-21 23:06:39 +0200

    final

--- lesson13/js/closure.js
+++ lesson13/js/closure.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://raw.githubusercontent.com/Lusyluh/Lusyluh.github.io/master/lesson13/temples.json';
+const requestURL = 'https://raw.githubusercontent.com/Lusyluh/Lusyluh.github.io/master/lesson13/closures.json';
 
 fetch(requestURL)
     .then(function (response){
@@ -7,29 +7,57 @@ fetch(requestURL)
     .then(function (jsonObject){
         console.table(jsonObject);
 
-        const temples = jsonObject['temples'];
+        const temples = jsonObject['closure'];
         
         for (let i = 0; i < temples.length;i++){
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2')
-            let announced = document.createElement('p')
-            let groundbreaking = document.createElement('p')
-            let dedicated = document.createElement('p')
-            let services = document.createElement('p')
-            let image = document.createElement('img')
+            if(temples[i].name == "Adelaide Australia Temple" || temples[i].name == "Accra Ghana Temple"|| temples[i].name == "Aba Nigeria Temple" ||temples[i].name == "Rexburg Idaho Temple"){
+                let closures = document.createElement('section');
+                let h2 = document.createElement('h2');
+                let h3 = document.createElement('h3');
+                let announced = document.createElement('p');
+                let groundbreaking = document.createElement('p');
+                let dedicated = document.createElement('p');
+                let image = document.createElement('img');
+                let thisYear = document.createElement('p');
+                let nextYear = document.createElement('h3');
+                let address = document.createElement('p');
+                let tel = document.createElement('p');
+                let info = document.createElement('span');
+            
 
-            h2.textContent = temples[i].name;
-            announced.textContent = 'Announced: ' + temples[i].announced;
-            groundbreaking.textContent = 'Ground Breaking: ' + temples[i].groundbreaking;
-            dedicated.textContent = 'Dedicated: ' + temples[i].dedicated;
-            services.textContent = 'Services: ' + temples[i].services[i];
-            image.setAttribute('src', temples[i].imageurl);
+                h2.textContent = temples[i].name;
+                image.setAttribute('src', temples[i].imageurl);
+                announced.textContent = 'Announced: ' + temples[i].announced;
+                groundbreaking.textContent = 'Ground Breaking: ' + temples[i].groundbreaking;
+                dedicated.textContent = 'Dedicated: ' + temples[i].dedicated;
+                address.textContent = 'Address: ' + temples[i].address;
+                tel.textContent = 'Phone: ' + temples[i].tel;
+                h3.textContent = '2019';
+                thisYear.textContent =  temples[i].thisYear;
+                nextYear.textContent = '2020'
 
-            card.appendChild(h2);
-            card.appendChild(announced)
-            card.appendChild(groundbreaking)
-            card.appendChild(dedicated)
-            card.appendChild(image)
-            document.querySelector('div.temples').appendChild(card);
+                info.appendChild(h2);
+                closures.appendChild(image)
+                info.appendChild(announced);
+                info.appendChild(groundbreaking);
+                info.appendChild(dedicated);
+                info.appendChild(address);
+                info.appendChild(tel);
+                info.appendChild(h3);
+                info.appendChild(thisYear);
+                info.appendChild(nextYear);
+                closures.appendChild(info);
+                
+                document.querySelector('div.temple_closure').appendChild(closures);
+
+                const twenty = temples[i]['nextYear'];
+                for (let j = 0; j < twenty.length; j++){
+                    let eachDate = document.createElement('p');
+
+                    eachDate.textContent = temples[i].nextYear[j];
+                    info.appendChild(eachDate);
+
+                }
+            }
         }
     });
--- lesson13/js/temples.js
+++ lesson13/js/temples.js
@@ -10,6 +10,7 @@ fetch(requestURL)
         const temples = jsonObject['temples'];
         
         for (let i = 0; i < temples.length;i++){
+                  
             let card = document.createElement('section');
             let h2 = document.createElement('h2')
             let announced = document.createElement('p')
@@ -17,19 +18,32 @@ fetch(requestURL)
             let dedicated = document.createElement('p')
             let services = document.createElement('p')
             let image = document.createElement('img')
+            let closures = document.createElement('section');
+            let h3 = document.createElement('h3')
+            let thisYear = document.createElement('p')
+            let nextYear = document.createElement('p')
 
             h2.textContent = temples[i].name;
+            image.setAttribute('src', temples[i].imageurl);
             announced.textContent = 'Announced: ' + temples[i].announced;
             groundbreaking.textContent = 'Ground Breaking: ' + temples[i].groundbreaking;
             dedicated.textContent = 'Dedicated: ' + temples[i].dedicated;
             services.textContent = 'Services: ' + temples[i].services[i];
-            image.setAttribute('src', temples[i].imageurl);
+            h2.textContent = 'TEMPLE CLOSURES'
+            thisYear.textContent = '2019: ' + temples[i].thisYear;
+            nextYear.textContent = '2020: ' + temples[i].nextYear[i];
+            
 
             card.appendChild(h2);
             card.appendChild(announced)
             card.appendChild(groundbreaking)
             card.appendChild(dedicated)
             card.appendChild(image)
+
+            closures.appendChild(h3);
+            closures.appendChild(thisYear)
+            closures.appendChild(nextYear)
+            document.querySelector('div.temple_closure').appendChild(closures);
             document.querySelector('div.temples').appendChild(card);
         }
     });
